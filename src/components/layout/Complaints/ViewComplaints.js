3cd7e54ff4797eec055cf252a5967bf68e033fed
febin <febnmathew98@gmail.com>
2023-07-10 20:29:21 +0530

    ViewComplaints component added sucessfully..modified sidebar (for Viewcomplaint navigation) and navigation (for Viewcomplaint routing).Also an unnoticable change ini viewFeedback component which doesnt effect the code..This is the day end commit dated- 10-07-23, Monday

--- src/components/layout/Complaints/ViewComplaints.js
+++ src/components/layout/Complaints/ViewComplaints.js
@@ -1,35 +1,35 @@
-import React, { useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
+import Sidebar from '../Sidebar';
+import Header from '../Header';
+import { Post } from '../../../service/Api';
 import { Link } from 'react-router-dom';
-import Sidebar from '../layout/Sidebar'
-import Header from '../layout/Header'
-import { Post } from '../../service/Api';
 
 
-function ViewFeedback() {
+function ViewComplaints() {
 
-    const [feedback, setFeedback] = useState([]);
+
+    const [complaints, setComplaints] = useState([]);
     const [refresh, setRefresh] = useState(0);
 
-    useEffect( ( )=> {
-       
-        Post("getfeedbacks", {tablename:"feedback"}).then((data) => {
-            setFeedback(data);
+    useEffect(()=> {
+
+        Post('getcomplaints', {tablename:'complaints'}).then((data)=> {
+            setComplaints(data);
+            console.log(data);
             
-        })
-       
+        });
+
     }, [refresh]);
 
-    const deleteFeedback = (id) => {
-        Post("delete", { tablename: "feedback", id: id }).then((data) => {
+    const deleteComplaint = (id) => {
+        Post("delete", { tablename: "complaints", id: id }).then((data) => {
           setRefresh((prev) => prev + 1);
         });
       };
-
-
-
-  return ( 
-    <>
-    <Sidebar />
+    
+  return (
+   <>
+   <Sidebar />
       <div className="content">
         <Header />
 
@@ -48,26 +48,31 @@ function ViewFeedback() {
                       <th scope="col">Name</th>
                       <th scope="col">Contact</th>
                       <th scope="col">Address</th>
-                      <th scope="col">Feedback</th>
+                      <th scope="col">Complaints</th>
                       <th scope="col">Action</th>
                     </tr>
                   </thead>
                   <tbody>
 
                     {
-                        feedback?.map((value, index) => {
+                        complaints?.map((complaint, index) => {
                             return (
                           <tr key={index}>
                           <th scope="row">{index +1}</th>
-                          <td>{value.username}</td>
-                          <td>{value.contact}</td>
-                          <td>{value.address}</td>
-                          <td>{value.feedback}</td>
+                          <td>{complaint.username}</td>
+                          <td>{complaint.contact}</td>
+                          <td>{complaint.address}</td>
+                          <td>{complaint.complaint}</td>
                           <td>
-                            
+                            <Link 
+                           state={{complaintid:complaint.complaintid}}
+                            className="btn btn-success mx-2"
+                            to='/replycomplaint'>
+                                  Reply
+                            </Link>
                            
                             <button
-                              onClick={() => deleteFeedback(value.fid)}
+                              onClick={() => deleteComplaint(complaint.complaintid)}
                               className="btn btn-danger"
                             >
                               Delete
@@ -87,9 +92,8 @@ function ViewFeedback() {
           </div>
         </div>
       </div>
-
-    </>
+   </>
   )
 }
 
-export default ViewFeedback
+export default ViewComplaints
